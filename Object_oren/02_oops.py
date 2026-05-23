d3aa7d85eabc255dc9ed7d5f403e007088d4eb73
Armaan Sharma <armaan2345sharma@gmail.com>
2026-05-23 10:20:01 +0000

    use of oops

---
+++ Object_oren/02_oops.py
@@ -0,0 +1,11 @@
+class remote():
+    passs
+class player():
+    def moveRight(self):
+        pass
+    def moveLeft(self):
+        pass
+remote1=remote()
+player1=player()
+if(remote1.isLeftPressed()):
+    player1.moveLeft()
