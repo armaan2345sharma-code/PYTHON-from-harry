45f9ec30cd3d8c6ecede0df1216bd2f48d94fc87
Armaan Sharma <armaan2345sharma@gmail.com>
2026-05-26 07:35:17 +0000

    A calculator that perfor square,cube and square root

---
+++ HW10/pr2c10.py
@@ -0,0 +1,13 @@
+#Calculator
+class calculation:
+    def square(self,num):
+        return num*num
+    def cube (self,num):
+        return num*num*num
+    def square_root(self,num):
+        return num**0.5
+num=int(input("Enter a number: "))
+cal=calculation ()
+print("Square of num is:",cal.square(num))
+print("Cube of num is:",cal.cube(num))
+print("Square root of num is:",cal.square_root(num))
