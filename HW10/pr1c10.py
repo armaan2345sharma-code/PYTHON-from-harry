7ace7ed4696d3dc9da62b641ea45d78a5f446c53
Armaan Sharma <armaan2345sharma@gmail.com>
2026-05-26 06:57:15 +0000

    class of company

---
+++ HW10/pr1c10.py
@@ -0,0 +1,13 @@
+class employes:
+    company="Microsoft"
+    salary=100000
+harshit=employes()
+conan=employes()
+namn=input("Enter the name of employee ")
+xc=int(input("Enter the salary of employees"))
+
+print(f"Company name is {harshit.company} and salary is {harshit.salary}")
+print(f"Company name is {conan.company} and salary is {conan.salary}")
+print(f"Company name is {namn} and salary is {xc}")
+
+
