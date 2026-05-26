7bc39a99f99670af433cf2a434bd60d6fa0524dd
Armaan Sharma <armaan2345sharma@gmail.com>
2026-05-26 07:45:24 +0000

    CALCULATOR with static method

--- HW10/pr2c10.py
+++ HW10/pr2c10.py
@@ -4,10 +4,14 @@ class calculation:
         return num*num
     def cube (self,num):
         return num*num*num
+    @staticmethod
+    def print_thanks():
+        print("Thanks for using this calculator ")
     def square_root(self,num):
         return num**0.5
 num=int(input("Enter a number: "))
 cal=calculation ()
+calculation.print_thanks()
 print("Square of num is:",cal.square(num))
 print("Cube of num is:",cal.cube(num))
 print("Square root of num is:",cal.square_root(num))
