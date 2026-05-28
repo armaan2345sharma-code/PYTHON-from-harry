9e9ba87bfc4e163c7dd2085d1f8638e437f92443
Armaan Sharma <armaan2345sharma@gmail.com>
2026-05-28 07:12:35 +0000

    inharitance_oops1

---
+++ Inharitance_oops/CW10.py
@@ -0,0 +1,19 @@
+class Employee:
+    company="Google"
+    def getInfo(self):
+        print("This is an Employee class")
+class Programmer(Employee):#This is inheritance 
+    language="Python"
+   # company="YouTube"
+    def getLanguage(self):
+        print("the language is",self.language)
+    def getInfo(self):
+        print("This is a Programmer class")#this will override the employee attribute
+e=Employee()
+e.getInfo()
+p=Programmer()
+p.getInfo()
+p.getLanguage()
+print (p.company)
+    
+     
