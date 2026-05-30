abe876f42f0f7da4822eece7e317939b186e72e4
Armaan Sharma <armaan2345sharma@gmail.com>
2026-05-30 05:13:58 +0000

    multilevel inharitance in python

---
+++ Inharitance_oops/multilevel_inh.py
@@ -0,0 +1,22 @@
+class person:
+    countary="india"
+    def takeBreath(self):
+        print("I am breathing")
+class Employee(person):
+    compny="Honda"
+    def getSalary(self):
+        print("Salary is 100k")
+    def takeBreath(self):
+        print("I ama an employee so i am luckily breathing")
+class Programmer(Employee):
+    company="Fiverr"
+    def getSalary(self):
+        print("No salary for programmer")
+    def takeBreath(self):
+        print("I am a programmer so i am breathing fine")
+p=person()
+p.takeBreath()
+pr=Programmer()
+print(pr.compny)
+e=Employee()
+e.takeBreath()
