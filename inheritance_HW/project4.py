5d61119b6f8bd879cef14d9bfb1887a47387e368
Armaan Sharma <armaan2345sharma@gmail.com>
2026-06-08 10:37:13 +0000

    learning overwri

--- inheritance_HW/project4.py
+++ inheritance_HW/project4.py
@@ -8,12 +8,17 @@ class vector:
             str1 += f" {i}a{index} +"
             index +=1
         return str1[:-1]
-    def __add__(self):
+    def __add__(self,vec2):
         newList=[]
         for i in range(len(self.vec)):
             newList.append(self.vec[i]+vec2.vec[i])
         return vector(newList)
+    def __mul__(self,vec2):
+        sum=0
+        for i in range(len(self.vec)):
+            sum += self.vec[i]*vec2.vec[i]
+        return sum
 v1= vector([1,4,])
 v2= vector ([1,6])
-print (v1)
-
+print (v1+v2)
+print (v1*v2)
